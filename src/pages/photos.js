8e87fdf67d9e416e93ebd0c9f1363c6779d58ec4
dloynaz <dloynaz@hotmail.com>
2020-06-24 00:06:18 -0600

    Create Photos Page

---
+++ src/pages/photos.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Photos() {
+  return (
+    <div className="Photos">
+      Photos
+      <br></br>
+      <img src="../../images/photo.jpg"></img>
+    </div>
+  );
+}
+
+export default Photos;
