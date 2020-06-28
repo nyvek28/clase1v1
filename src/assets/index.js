c1f8c193b6477018861944a3a08c6d7f7a7dab35
dloynaz <dloynaz@hotmail.com>
2020-06-28 14:31:30 -0600

    Basic style

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export { default as El_Greco } from './El_Greco.jpg'
+export { default as execution } from './execution.jpg'
+export { default as Judit } from './Judit.jpg'
+export { default as Kripal } from './Kripal.jpg'
+export { default as Weyden } from './Weyden.jpg'
+
